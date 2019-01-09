0d3c1194f0bf8d534c3955744e9acac6af5d4565
XXX <prashantkr314@gmail.com>
2019-01-09 16:11:36 +0530

    making connection Front-end to server

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+const socket = require('socket.io');
+
 
 // App setup
 const app = express();
@@ -14,6 +16,15 @@ app.use(express.static('public'));
 
 const server = app.listen(port, ()=>{
       console.log('\x1b[34m%s\x1b[0m',`HTTP Server @ ${port}`);
+});
+
+
+// socket.io setup
+const io = socket(server);
+
+io.on('connection', (socket) =>{
+      console.log('\x1b[35m%s\x1b[0m',`${socket.id} connection made!`);
+      // console.log(socket);
 });
 
 
---
+++ public/chat.js
@@ -0,0 +1,7 @@
+'use strict';
+// ==>> REMEMBER : this is running on to the front-end
+// and we need to seprate our frontend from the backend
+
+// make connection from front-end to the server
+
+let socket = io.connect('http://localhost:5000');
